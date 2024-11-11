a9707f6a7c5688716d7528a843435f6d3cbc5b52
PharingWell <pharingwell@gmail.com>
2024-11-11 03:49:29 -0500

    Implemented set action info for UI

--- Source/NexusTriumphant/Public/AbilitySystem/NActionHelper.h
+++ Source/NexusTriumphant/Public/AbilitySystem/NActionHelper.h
@@ -1,22 +1,34 @@
 #pragma once
 
+#include "NActionHelper.generated.h"
+
+class UTexture2D;
+
 DECLARE_LOG_CATEGORY_EXTERN(LogActionSystem, Verbose, All);
 
-UENUM()
+UENUM(Blueprintable)
 enum ENAbilityAction //Nexus Ability Action: What action the play is taking to activate these things
 {
-	INVALID = -1,
-	MOVETO,
-	ATTACK,
-	ABILITY1,
-	ABILITY2,
-	ABILITY3,
-	ULTIMATE,
-	TRAIT,
-	ADDT1,
-	ADDT2,
+	INVALID = 0 UMETA(DisplayName="Invalid"),
+	MOVETO      UMETA(DisplayName="MoveTo"),
+	ATTACK      UMETA(DisplayName="Attack"),
+	ABILITY1    UMETA(DisplayName="Ability1"),
+	ABILITY2    UMETA(DisplayName="Ability2"),
+	ABILITY3    UMETA(DisplayName="Ability3"),
+	ULTIMATE    UMETA(DisplayName="AbilityUltimate"),
+	TRAIT       UMETA(DisplayName="AbilityTrait"),
+	ADDT1       UMETA(DisplayName="AbilityAddt1"),
+	ADDT2		UMETA(DisplayName="AbilityAddt2"),
 };
 
-
-
+USTRUCT(Blueprintable, BlueprintType)
+struct NEXUSTRIUMPHANT_API FNAbilityIconDef
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(Blueprintable, BlueprintReadWrite)
+	TObjectPtr<UTexture2D> Icon;
+	UPROPERTY(Blueprintable, BlueprintReadWrite)
+	FText Keybinding;
+};
 
